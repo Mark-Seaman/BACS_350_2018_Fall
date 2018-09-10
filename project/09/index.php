1b8422803cc056fc4f218dc5e8b6e549b7c3c205
ghasch317 <43016264+ghasch317@users.noreply.github.com>
2018-09-10 15:24:40 -0600

    pro update

---
+++ project/09/index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>UNC BACS 350 - Demo Server</title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        <header>
+            <img src="Bear3.png" alt="Bear Logo">
+            <h1>BACS 350 - Demo Server</h1>
+        </header>
+        <main>
+
+            <h2> Website Projects</h2>
+            <ul>
+                <li>
+                    <a href="../project/08/index.php">Exercise #8</a>
+                </li>
+            </ul>
+
+            <h2>Code Templates</h2>
+            <ul>
+                <li>
+                    <a href="https://seamansguide.com/guide/PhpApps/templates/page.php">Simple Page</a>
+                </li>
+                <li>
+                    <a href="https://seamansguide.com/guide/PhpApps/templates/header.php">Header for Page</a>
+                </li>
+                <li>
+                    <a href="https://seamansguide.com/guide/PhpApps/templates/footer.php">Footer for Page</a>
+                </li>
+                <li>
+                    <a href="https://seamansguide.com/guide/PhpApps/templates/files.php">Files Utilities</a>
+                </li>
+                <li>
+                    <a href="https://seamansguide.com/guide/PhpApps/templates/dirlist.php">Directory Listing</a>
+                </li>
+            </ul>
+            
+            <hr>
+            
+            <li><a href="../project/09/test.php">Test for index.php</a></li>
+            
+            <hr>
+            
+            <?php
+                $validator = 'https://validator.w3.org/nu/';
+                echo "validator = $validator";
+            ?>
+            
+            
+            
+        </main>
+
+    </body>
+</html>
+
--- project/09/test.php
+++ project/09/test.php
@@ -1,22 +1,32 @@
-<?php
-    
-    include '../header.php';
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <?php
 
-    // Decide on the domain and page to test
-    $domain = "https://unco-bacs.org";
-    $page = "$domain/bacs_350/demo/index.php";
+            include '../header.php';
 
-    // Create a link for the page
-    echo "<h2>Test Page for $page</h2>
-        <p>Start simple but start now.</p>
-        <p>Make sure that the page exists and has valid HTML.";
+            // Decide on the domain and page to test
+            $domain = "https://unco-bacs.org";
+            $page = "$domain/bacs_350/demo/index.php";
 
-    echo '<h3>Page Exists</h3>
-        <p><a href="' . $page . '">' . $page . '</a></p>';
+            // Create a link for the page
+            echo "<h2>Test Page for </h2>
+                <p>Start simple but start now.</p>
+                <p>Make sure that the page exists and has valid HTML.";
 
-    // Run your page through the validator
-    echo '<h3>Valid HTML</h3>
-        <p><a href="http://validator.w3.org">HTML Validator</a></p>'
+            echo '<h3>Page Exists</h3>
+                <p><a href="' . $page . '">' . $page . '</a></p>';
+
+            // Run your page through the validator
+            echo '<h3>Valid HTML</h3>
+                <p><a href="http://validator.w3.org">HTML Validator</a></p>'
+
+            include '../footer.php';
+        ?>
+
+        <hr>
+
+        <a href="https://validator.w3.org/nu/?doc=https%3A%2F%2Funco-bacs.org%2Fbacs_350%2Fdemo%2Findex.php">HTML Validator</a>
         
-    include '../footer.php';
-?>
+    </head>
+</html>
