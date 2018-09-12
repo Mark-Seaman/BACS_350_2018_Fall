ff592beff382d1406c50981cc61a66a0f8278cc3
ghasch317 <43016264+ghasch317@users.noreply.github.com>
2018-09-12 15:22:26 -0600

    pro 10

---
+++ subscribers/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>UNC BACS 350 - Demo Server</title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        <header>
+            <img src="Bear3.png" alt="Bear Logo">
+            <h1>BACS 350 - Demo Server</h1>
+        </header>
+        <main>
+
+            <h2> Website Projects</h2>
+            <ul>
+                <li>
+                    <a href="project/09/index.php">Exercise #09</a>
+                </li>
+
+            </ul>
+
+        </main>
+
+    </body>
+</html>
+
