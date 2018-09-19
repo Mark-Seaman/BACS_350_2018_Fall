bf8ca6754e2c9ee46d5b916c802250557a26db0e
ghasch317 <43016264+ghasch317@users.noreply.github.com>
2018-09-19 14:25:12 -0600

    pro 13 start

---
+++ project/13/index.php
@@ -0,0 +1,58 @@
+
+ <!DOCTYPE html>
+<html lang="en">
+    
+    <head> 
+
+        <meta charset="UTF-8">
+        <title>
+            BACS 350 - Project #13 - Book App
+        </title>
+        <link rel="stylesheet" href="style.css">
+
+    </head>
+    
+    <body>
+
+        <header>
+            <h1>
+                <img src="Bear3.png" alt="Bear Logo"/>
+
+                BACS 350 - Project #13 - Book App            </h1>
+        </header>
+        <main>
+            <a href="../bacs_350">Home</a>
+            <h1>My Book List</h1>
+            <h2>Books in List</h2>
+            <ul>
+                <li>1, Tale of Two Cities, Charles Dickens</li>
+                <li>2, Dummy Title, Dummy Author</li>
+                <li>3, Web Dev, Mark</li>
+                <li>4, Change in Grading Algorithm, Mark</li>
+            </ul>
+            
+            <a href="insert.php">Add a Book</a>
+            <form action="insert.php" method="get">
+        
+        <p><label>Title:</label> &nbsp; <input type="text" name="title"></p>
+        <p><label>Author:</label> &nbsp; <input type="text" name="author"></p>
+        <p><label>Summary:</label> &nbsp; <input type="text" name="summary"></p>
+        
+        <p><input type="submit" value="Add Book"/></p>
+        
+    </form>
+            
+        <hr>
+            <h2>Test Page for project/13/index.php</h2>
+            <p>Start simple but start now.</p>
+            <p>Make sure that the page exists and has valid HTML.
+            <h3>Page Exists</h3>
+            <p><a href="https:/http://ghaschbacs350.com/bacs_350/project/13/index.php">project/13/index.php</a></p>
+            <h3>Valid HTML</h3><p><a href="http://validator.w3.org">HTML Validator</a></p>
+            <p><a href="http://validator.w3.org/nu/?doc=https://http://ghaschbacs350.com/bacs_350/project/13/index.php">Validate Page: project/13/index.php</a></p>
+        </main>
+        
+    </body>
+    
+</html>
+
