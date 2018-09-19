609844b042002f903ace212af58fdfabf955ca6f
ghasch317 <43016264+ghasch317@users.noreply.github.com>
2018-09-19 11:56:40 -0600

    pro 10 11 updated

--- project/11/index.php
+++ project/11/index.php
@@ -1,38 +1,58 @@
 <!DOCTYPE html>
 <html lang="en">
     <head>
-        <title>Project 11</title>
-        <?php
-            include 'header.php';
-        ?>
+
+        <meta charset="UTF-8">
+        <title>
+           Project #11
+        </title>
+        <link rel="stylesheet" href="style.css">
+
     </head>
-    
     <body>
+
         <header>
-            <img src="../Bear3.png" alt="Bear Logo">
-            <h1>BACS 350 - Demo Server</h1>
+            <h1>
+                <img src="Bear3.png" alt="Bear Logo"/>
+
+                Project #11
+            </h1>
         </header>
+        
         <main>
+            <a href="../../index.html">Home</a>
+    <h2>Subscriber Application</h2>
 
-            <h2>Project 11</h2>
-            <hr>
-            <?php
-            
-                require 'db.php';
-                require 'select.php';
-                require 'test.php';
-            ?>
-            <hr>
-            
-            
-        </main>
+    <!-- Display subscriber records
+    require 'select.php';-->
 
-    </body>
+
+    <h2>Register</h2>
+
+    <form action="test.php" method="post">
+        
+        <p><label>Name:</label> &nbsp; <input type="text" name="name"></p>
+        <p><label>Email:</label> &nbsp; <input type="text" name="email"></p>
+        
+        <p><input type="submit" value="Register"/></p>
+        
+    </form>
+            
+    <hr>
+            
+    <p>Make sure that the page exists and has valid HTML.<h3>Page Exists</h3>
+            
+    <p><a href="http://http://ghaschbacs350.com/bacs_350/project/11/index.php">/bacs_350/project/11/index.php</a></p><h3>Valid HTML</h3><p>
+            
+    <hr>
+            
+    <p><a href="http://validator.w3.org">HTML Validator</a></p>
     
-    <footer>
-        <?php
-            include 'footer.php';
-        ?>
-    </footer>
+    <p><a href="http://validator.w3.org/nu/?doc=http://ghaschbacs350.com/bacs_350/project/11/index.php">Validate Page:         ghaschbacs350.com/bacs_350/project/11/index.php</a></p>
+
+
+
+        </main>
+    </body>
 </html>
 
